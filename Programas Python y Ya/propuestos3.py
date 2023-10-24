707276c3a31504b04b480782c8e870575c67dab5
Diego Govea <87109033+diegogovea@users.noreply.github.com>
2023-10-24 16:54:02 -0600

    Update propuestos3.py

--- Programas Python y Ya/propuestos3.py
+++ Programas Python y Ya/propuestos3.py
@@ -15,3 +15,24 @@ else:
         print("Regular")
     else:
         print("Reprobado")
+
+#Programa 2
+numero1 = float(input("Ingrese el primer número: "))
+numero2 = float(input("Ingrese el segundo número: "))
+numero3 = float(input("Ingrese el tercer número: "))
+
+mayor = numero1
+
+if numero2 > mayor:
+    mayor = numero2
+
+if numero3 > mayor:
+    mayor = numero3
+
+print("El número mayor es:", mayor)
+
+#Una forma de hacerlo con python sin tener que pensar en la lógica es
+
+numero_mayor = max(numero1, numero2, numero3)
+print("El número mayor es:", numero_mayor)
+
