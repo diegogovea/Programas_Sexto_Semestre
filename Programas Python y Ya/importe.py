7e90b431124409ad9bbefd2dbb8d88cb5b2ce1a8
Diego Govea <87109033+diegogovea@users.noreply.github.com>
2023-10-24 13:35:49 -0600

    Update importe.py

--- Programas Python y Ya/importe.py
+++ Programas Python y Ya/importe.py
@@ -1,5 +1,32 @@
+#TEMA: ESTRUCTURA DE PROGRAMACIÓN SECUENCIAL
+#PROBLEMAS PROPUESTOS
+#DIEGO LEON GOVEA ORTIZ
+
+#1. Importe
 precio=int(input("ingrese el precio:"))
 cantidad=int(input("ingrese la cantidad:"))
 importe=precio*cantidad
 print("El importe es: ")
 print(importe)
+
+#2. Perimetro
+lado=int(input("ingrese el lado de un cuadrado pero en fa:"))
+perimetro=lado*4
+print("El perimetro es: ")
+print(perimetro)
+
+#3. Suma y Mult
+num1=int(input("Ingrese numero 1: "))
+num2=int(input("Ingrese numero 2: "))
+num3=int(input("Ingrese numero 3: "))
+num4=int(input("Ingrese numero 4: "))
+suma=num1+num2
+mult=num3*num4
+print("La suma de los primeros 2 es: ",suma)
+print("La multi de los ultimos 2 es: ",mult)
+
+#4. Promedio
+suma2=num1+num2+num3+num4
+promedio=suma2/4
+print("La suma de todos los numeros es: ",suma2)
+print("el promedio es: ",promedio)
