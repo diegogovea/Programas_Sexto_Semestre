f6347a3b64eb5ac1ac829497e94041b61e8ccf49
Diego Govea <87109033+diegogovea@users.noreply.github.com>
2023-10-31 14:14:46 -0600

    Create propuestos4.py

---
+++ Programas Python y Ya/propuestos4.py
@@ -0,0 +1,17 @@
+#TEMA 4: Estructura de datos tipo lista
+#PROBLEMAS PROPUESTOS
+#DIEGO LEON GOVEA ORTIZ
+
+#Programa 1
+
+lista = [100,200,300,50,40,200,20]
+
+x=0
+mayores=0
+while x<len(lista):
+    if lista[x]>100:
+        print("El valor ",lista[x]," es mayor a 100")
+        mayores+=1
+    x=x+1
+
+print("Numeros mayores a 100: ",mayores)
