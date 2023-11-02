0efa918b824658c1d8239edd0f6a8b4e70d36168
Diego Govea <87109033+diegogovea@users.noreply.github.com>
2023-11-01 20:28:39 -0600

    Update propuestos5.py

--- Programas Python y Ya/propuestos5.py
+++ Programas Python y Ya/propuestos5.py
@@ -14,3 +14,23 @@ for a in range(len(oracion)):
     a+=1
 
 print("Hay ",e," espacios")
+
+#Programa 2: Vocales
+
+oracion = input("Ingresa una cadena ---> ")
+
+print(oracion)
+a=0
+e=0
+o=oracion.lower()
+
+for a in range(len(o)):
+    if o[a]   == "a":e+=1
+    elif o[a] == "e":e+=1
+    elif o[a] == "i":e+= 1
+    elif o[a] == "o":e+= 1
+    elif o[a] == "u":e+= 1
+
+    a+=1
+
+print("Hay ",e," vocales")
